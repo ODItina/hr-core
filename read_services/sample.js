693b7e3624ceb7beadee2b031fa15de29f1e248e
Dominic Thompson <onlythompson@gmail.com>
2014-10-02 12:18:29 +0100

    includes service directory and samples

---
+++ read_services/sample.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dominic on 02-Oct-2014.
+ */
+
+//var sample = require('../models/sample');
+//
+//exports.getSamples = function(err, result){
+//
+//};
+//
+//exports.getSample = function(err, result){
+//
+//};
---
+++ write_services/sample.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Dominic on 02-Oct-2014.
+ */
+
+//var sample = require('../models/sample');
+//
+//exports.createSample = function(err, result){
+//
+//};
+//
+//exports.updateSample = function(err, result){
+//
+//};
+// exports.deleteSample = function(err, result){
+//
+//};
